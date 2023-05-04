fd6634d638a2b7054776835de64c839289cbc539
olejenkasmirnov <mr.smirnov.oleg2001@mail.ru>
2023-05-04 23:45:17 +0300

    Added LRUCache

---
+++ hw1/LRUTest.py
@@ -0,0 +1,10 @@
+from LRUCache import LRUCache
+
+
+cache = LRUCache(3)
+cache.set('Jesse', 'Pinkman')
+cache.set('Walter', 'White')
+cache.set('Jesse', 'James')
+print(cache.get('Jesse'))  # вернёт 'James'
+cache.rem('Walter')
+print(cache.get('Walter'))  # вернёт ''
